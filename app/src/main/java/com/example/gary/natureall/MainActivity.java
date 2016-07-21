68f79a4f08e9877f189f2221863dbb99a753fdb3
handspike <garymadden01@gmail.com>
2016-07-21 11:26:45 +0100

    onClicks Added

---
+++ app/src/main/java/com/example/gary/natureall/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.gary.natureall;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onSearchAnimalClick(View view) {
+    }
+
+    public void onSearchPlantClick(View view) {
+    }
+
+    public void onUploadPictureClick(View view) {
+    }
+}
